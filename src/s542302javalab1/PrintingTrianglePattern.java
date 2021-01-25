1ba65642f712248661b7e61c90c6a34a3b61b5f0
AbhinayOladri <77645595+AbhinayOladri@users.noreply.github.com>
2021-01-25 16:52:27 -0600

    S542302JavaLab1
    Pushing First Assignment Code to Github.

---
+++ src/s542302javalab1/PrintingTrianglePattern.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s542302javalab1;
+
+/**
+ *
+ * @author S542302
+ */
+public class PrintingTrianglePattern {
+    
+    public static void main(String[] args) {
+    System.out.println("*");
+    System.out.println("* *");
+    System.out.println("* * *");
+    System.out.println("* * * *");
+    System.out.println("* * * * *");
+
+}
+}
+
+
---
+++ src/s542302javalab1/S542302JavaLab1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s542302javalab1;
+
+/**
+ *
+ * @author S542302
+ */
+public class S542302JavaLab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi, This is Abhinay");
+        System.out.println();
+        System.out.println("I Can Write Java Now");
+        System.out.println();
+        System.out.println("Isn't that Wonderful");
+
+                
+    }
+    
+}
+
+
+
+
+
+
